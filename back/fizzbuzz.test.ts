3d4e4ba5ff99ae9b1b64b2d72205593eb2cf0355
Patrice CAVEZZAN <pcavezzan@gmail.com>
2021-06-14 20:29:15 +0200

    Implementation of fizzbuzz.

--- back/fizzbuzz.test.ts
+++ back/fizzbuzz.test.ts
@@ -2,6 +2,11 @@ import {assertEquals} from "https://deno.land/std@0.98.0/testing/asserts.ts";
 import {fizzbuzz} from './fizzbuzz.ts';
 
 Deno.test("should return the entry number by default", () => {
+    const number = 1;
+
+    const result = fizzbuzz(number);
+
+    assertEquals("1", result);
 });
 
 Deno.test("should return FIZZ if multiple of 3", () => {
@@ -14,10 +19,18 @@ Deno.test("should return FIZZ if multiple of 3", () => {
 
 // Fully fledged test definition, longer form, but configurable (see below)
 Deno.test("should return BUZZ if multiple of 5", () => {
+    const number = 5;
+
+    const result = fizzbuzz(number);
+
+    assertEquals("BUZZ", result);
 });
 
 
 Deno.test("should return FIZZBUZZ if multiple of 3 and 5", () => {
-    const x = 1 + 2;
-    assertEquals(x, 4);
+    const number = 15;
+
+    const result = fizzbuzz(number);
+
+    assertEquals("FIZZBUZZ", result);
 });
--- back/fizzbuzz.ts
+++ back/fizzbuzz.ts
@@ -1,3 +1,15 @@
 export const fizzbuzz = (number: number): string => {
-    return 'FIZZ';
+    let result = '';
+
+    if (isFizz(number)) {
+        result = 'FIZZ';
+    }
+
+    if (isBuzz(number)) {
+        result += 'BUZZ';
+    }
+    return result ? result : number.toString();
 };
+
+const isFizz = (number: number): boolean => number % 3 === 0
+const isBuzz = (number: number): boolean => number % 5 === 0
