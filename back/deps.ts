65aeeed679cf62aceb239a175c1806c9d3eacc79
Sylvain Delafoy <sylvain.delafoy@zenika.com>
2021-06-30 00:03:30 +0200

    feat(back) ajout d'un log de démarrage avec le port

--- back/deps.ts
+++ back/deps.ts
@@ -1,1 +1,2 @@
 export { Application, Router } from "https://deno.land/x/oak@v7.6.3/mod.ts";
+export * as log from "https://deno.land/std@0.99.0/log/mod.ts";
--- back/main.ts
+++ back/main.ts
@@ -1,3 +1,8 @@
 import { app } from "./app.ts";
+import { log } from "./deps.ts";
+
+app.addEventListener("listen", (evt) => {
+  log.info(`Serveur en cours sur http://localhost:${evt.port}`);
+});
 
 await app.listen({ port: 8000 });
