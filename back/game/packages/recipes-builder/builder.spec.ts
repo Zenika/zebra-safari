7353f1dcb1848e11304c6771ee59999fd318e170
Patrice CAVEZZAN <pcavezzan@gmail.com>
2021-10-25 20:43:06 +0200

    feat(game): added some features and test new deno nested test

--- back/game/packages/recipes-builder/builder.spec.ts
+++ back/game/packages/recipes-builder/builder.spec.ts
@@ -1,5 +1,12 @@
-import { description, assertEquals, assertThrows } from "./deps-test.ts";
-import { Product, Resource, productBuilder, Type, Recipe } from "./builder.ts";
+import { assertEquals, assertThrows, description } from "./deps-test.ts";
+import {
+  Product,
+  productBuilder,
+  Recipe,
+  Resource,
+  sortResources,
+  Type,
+} from "./builder.ts";
 import { Durability } from "./builder.ts";
 import { assertNotEquals } from "./deps-test.ts";
 
@@ -34,20 +41,72 @@ Deno.test(
     const omelette: Product = productBuilder([gruyere, oeuf], omeletteRecipe);
 
     assertEquals(product, omelette);
-  }
+  },
 );
 
 Deno.test(
   description({
     name: "Should combine two Resource into one Product",
-    given: "Two valid Resources that match a Recipe",
-    should: "Build the right Product",
+    given: "Two valid Resources that doesn't match the Recipe",
+    should: "Throw the right error (content)",
   }),
   () => {
     assertThrows(
       () => productBuilder([gruyere, gruyere], omeletteRecipe),
       undefined,
-      "Provided resources don't match the recipe ingredients."
+      "Provided resources doesn't match the recipe ingredients.",
     );
-  }
+  },
+);
+
+Deno.test(
+  description({
+    name: "Should combine three Resource into one Product",
+    given: "Three valid Resources that doesn't match the Two Resources Recipe",
+    should: "Throw the right error (size)",
+  }),
+  () => {
+    assertThrows(
+      () => productBuilder([gruyere, gruyere, oeuf], omeletteRecipe),
+      undefined,
+      "Provided resources list doesn't match the recipe ingredients size.",
+    );
+  },
+);
+
+Deno.test(
+  description({
+    name: "Should combine two Resource into one Product",
+    given: "Two valid Resources that match a Recipe in another order",
+    should: "Build the right Product",
+  }),
+  () => {
+    const omelette: Product = productBuilder([oeuf, gruyere], omeletteRecipe);
+
+    assertEquals(product, omelette);
+  },
+);
+
+Deno.test(
+  description({
+    name: "Should sort two Resource by type and name",
+    given: "Two valid Resources",
+    should: "Return the expected order",
+  }),
+  async (t) => {
+    await t.step("first resource is the same as the second", () => {
+      const result: number = sortResources(oeuf, oeuf);
+      assertEquals(result, 0);
+    });
+
+    await t.step("first resource should stay first", () => {
+      const result: number = sortResources(oeuf, gruyere);
+      assertEquals(result, 1);
+    });
+
+    await t.step("first resource should move last", () => {
+      const result: number = sortResources(gruyere, oeuf);
+      assertEquals(result, -1);
+    });
+  },
 );
