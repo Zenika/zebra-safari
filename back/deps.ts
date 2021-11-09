9de2d31c3cb8729cf1ec00f6ab8803edb78a359c
matthiasferraina <matthias.ferraina@gmail.com>
2021-11-09 20:11:43 +0100

    refactor(back): centralize dependencies

--- back/deps.ts
+++ back/deps.ts
@@ -1,2 +1,6 @@
 export { Application, Router } from "https://deno.land/x/oak@v9.0.1/mod.ts";
 export { config } from "https://deno.land/x/dotenv@v3.0.0/mod.ts";
+export {
+  cloneDeep,
+  zip,
+} from "https://deno.land/x/lodash@4.17.15-es/lodash.js";
