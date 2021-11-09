7e3b3700e4e605ed02542ff84ec7ddb98837693d
matthiasferraina <matthias.ferraina@gmail.com>
2021-11-09 20:10:59 +0100

    refactor(back): removed fizzbuzz

--- back/app.ts
+++ back/app.ts
@@ -1,19 +1,6 @@
-import { Application, Router } from "./deps.ts";
-import { fizzbuzz } from "./fizzbuzz.ts";
-
-const router = new Router();
-router.get("/fizzbuzz/:number", (context) => {
-  if (context?.params?.number) {
-    const number = +context.params.number;
-    const result = fizzbuzz(number);
-    context.response.body = {
-      number,
-      result,
-    };
-    context.response.status = 200;
-  }
-});
+import { builderRouter } from "./controllers/builder/index.ts";
+import { Application } from "./deps.ts";
 
 export const app = new Application();
-app.use(router.routes());
-app.use(router.allowedMethods());
+app.use(builderRouter.routes());
+app.use(builderRouter.allowedMethods());
